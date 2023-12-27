d8a1f7326ee388eb8b4d2b59dbb41b2c6e015cd0
saurabh kumar <saurabhkumar9493@gmail.com>
2023-12-27 15:20:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,75 @@
+// alert("hello")
+// alert("bye")
+
+// prompt("hello")
+// prompt("thanku for wisiting on my website")
+
+
+
+// console.log(12);
+// console.log("hello")
+//   let const var
+
+// let a = 12;
+//  var b = 20;
+//  const c = 30;
+
+//  console.log(a,b,c)
+
+
+// data types in js
+
+// 1.Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. Bigint
+// 7. Symbol
+
+// let a = 1;
+// let b = 10;
+
+// 2. string
+
+// let a = 'abc'
+// let b = "ashu"
+// let c = 123
+
+// console.log(typeof(a))
+
+// 3. Boolean
+
+// console.log(4>22);
+
+// let a = null;
+//  a = 10
+// console.log(a);
+
+
+// age = age+1;
+// age+=1
+
+// age = age-1;
+// age-=1
+
+// age = age*1;
+// age*=1
+
+// uniary operator
+
+// let a = 10;
+
+// let b = ++a;  //a+1
+// console.log(a);
+ 
+// let a = 10;
+
+// let b = a++;  //a+1
+// console.log(b);
+
+
+let str = 'arushi'
+// console.log(str);
+// console.log(str.length);
+console.log(str[6]);
